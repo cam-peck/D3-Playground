2274f5076b29d362c29d4ff31e68f89a2309bda7
Cameron <cameronpeck317@gmail.com>
2022-10-31 19:16:21 +0000

    complete svg face compartmentalization w/components

--- svg-face/src/index.jsx
+++ svg-face/src/index.jsx
@@ -1,37 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Face } from './face';
 
 const width = 960;
 const height = 500;
-const centerX = width / 2;
-const centerY = height / 2;
-const strokeWidth = 20;
-const eyeOffsetX = 90;
-const eyeOffsetY = 90;
-const eyeRadius = 50;
 
 const App = () => {
   return (
-    <svg width={width} height={height} >
-      <circle
-        cx={centerX}
-        cy={centerY}
-        r={centerY - strokeWidth / 2}
-        fill="yellow"
-        stroke="black"
-        strokeWidth={strokeWidth}
-      />
-      <circle
-        cx={centerX - eyeOffsetX}
-        cy={centerY - eyeOffsetY}
-        r={eyeRadius}
-      />
-      <circle
-        cx={centerX + eyeOffsetX}
-        cy={centerY - eyeOffsetY}
-        r={eyeRadius}
-      />
-    </svg>
+    <Face
+      width={width}
+      height={height}
+      centerX= {width / 2}
+      centerY = {height / 2}
+      strokeWidth = {20}
+      eyeOffsetX = {90}
+      eyeOffsetY = {90}
+      eyeRadius = {40}
+      mouthWidth = {20}
+      mouthRadius = {140}
+    />
   );
 };
 
